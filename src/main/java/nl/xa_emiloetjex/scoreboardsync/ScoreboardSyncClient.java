ce43eabe283ba1e717c666f5ac745662d6ac8ecb
Amy <robloxegshgp@gmail.com>
2024-10-21 10:44:02 +0200

    Initial commit

---
+++ src/main/java/nl/xa_emiloetjex/scoreboardsync/ScoreboardSyncClient.java
@@ -0,0 +1,10 @@
+package nl.xa_emiloetjex.scoreboardsync;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ScoreboardSyncClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/nl/xa_emiloetjex/scoreboardsync/ScoreboardSyncDataGenerator.java
@@ -0,0 +1,11 @@
+package nl.xa_emiloetjex.scoreboardsync;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ScoreboardSyncDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
